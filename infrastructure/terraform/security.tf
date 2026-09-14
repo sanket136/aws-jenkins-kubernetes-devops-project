3bb0d0fe187f4ea01714ec258e93b14b560384b1
sanket <sanketpatil1306@gmail.com>
2026-09-14 16:17:14 +0530

    Complete Terraform and Ansible infrastructure automation

--- infrastructure/terraform/security.tf
+++ infrastructure/terraform/security.tf
@@ -29,6 +29,14 @@ resource "aws_security_group" "terraform_master_sg" {
     self        = true
   }
 
+  ingress {
+    description = "Application NodePort"
+    from_port   = 30010
+    to_port     = 30010
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port   = 0
